865f6f9b136e504fa6f97f3b737ff156afc37d96
Ilya <lord-ilja@yandex.ru>
2022-01-27 22:03:12 +0300

    Отправка форм + pageup

--- src/js/script.js
+++ src/js/script.js
@@ -88,7 +88,34 @@ $(document).ready(function(){
 // Mask input 
 
   $('input[name=phone]').mask("+7(999) 999-99-99");
+
+// Mailer
    
+  $('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
+
+// Pageup + scroll
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 600) {
+    $('.pageup').fadeIn();
+  } else {
+    $('.pageup').fadeOut();
+  }
+});
 
 });
 
