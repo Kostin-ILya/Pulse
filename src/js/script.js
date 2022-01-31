f9398c6a40658f97db7a0e6cbda60aad16bfe379
Ilya <lord-ilja@yandex.ru>
2022-01-31 05:12:00 +0300

    Favicon+modal_mini320+validate+phpmailer (bug fixed)

--- src/js/script.js
+++ src/js/script.js
@@ -95,6 +95,9 @@ $(document).ready(function(){
    
   $('form').submit(function(e) {
     e.preventDefault();
+    if (!$(this).valid()) {
+      return;
+    }
     $.ajax({
         type: "POST",
         url: "mailer/smart.php",
